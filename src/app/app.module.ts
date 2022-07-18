970e7e611090d5e0598d165f4915582e945b12da
Rodrigo Montero Luna - Ceiba Software <rodrigo.montero@ceiba.com.co>
2022-07-18 00:49:15 -0500

    Eliminado a producto del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from '@home/home.component';
-import { ProductoModule } from '@producto/producto.module';
+
 import { CoreModule } from '@core/core.module';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,7 +26,6 @@ import { CrearFacturaComponent } from './feature/factura/components/crear-factur
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductoModule,
     CoreModule,
     HttpClientModule,
     FormsModule,
